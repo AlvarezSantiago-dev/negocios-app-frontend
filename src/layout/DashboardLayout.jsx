e388fefcfce3f84de62c3626a8471765ead9d203
Santi <santiagoalvarezc5@gmail.com>
2025-12-06 19:29:30 -0300

    update

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/SideBar.jsx";
+
 import Navbar from "../components/Navbar";
 
 export default function DashboardLayout() {
