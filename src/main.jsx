f58082eb1cf505befb3cc35c656c58b9ef534f24
Santi <santiagoalvarezc5@gmail.com>
2025-12-13 09:25:33 -0300

    Se agrego la importacion de styles/ print.css al main.jsx para darle styles al codigo de barra

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/print.css";
 import { DashboardProvider } from "./context/DashboardProvider";
 import AuthInitializer from "./context/AuthInitializer";
 import { BrowserRouter } from "react-router-dom";
